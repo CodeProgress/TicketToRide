0664618de3e3961d779207377fa1be9018e88cd5
codeprogress <codeprogress1@gmail.com>
2013-11-11 21:01:26 -0500

    Commit Reformatting of Player Class

--- TTRPlayer.py
+++ TTRPlayer.py
@@ -1,54 +1,61 @@
 import collections
 
-class Player:
+class Player(object):
     
-    def __init__(self, startingHand, startingTickets, playerBoard, playerPosition, numTrains):
-        '''orderNumber: int
+    def __init__(self, 
+                 startingHand, 
+                 startingTickets, 
+                 playerBoard, 
+                 playerPosition, 
+                 numTrains
+                 ):
+        """orderNumber: int
         startingHand: list
         startingTickets: list
         playerBoard: PlayerBoard object from the TTRBoard module
         playerPosition: int
-        '''
-        self.name = '' #ask for them to enter it on first turn
+        """
+        self.name           = '' #ask for them to enter it on first turn
         
         #implimented as a collection to avoid O(n) hand.remove(x)
-        self.hand = collections.Counter(startingHand)
-        self.tickets = {x:False for x in startingTickets}
-        self.numTrains = numTrains
-        self.points = 0
+        self.hand           = collections.Counter(startingHand)
+        
+        self.tickets        = {x:False for x in startingTickets}
+        self.numTrains      = numTrains
+        self.points         = 0
         self.playerPosition = playerPosition
         
         #custom board to represent
-        self.playerBoard = playerBoard
+        self.playerBoard    = playerBoard
                     
     def removeCardsFromHand(self, color, numColor):
-        '''removes one ore more cards from hand
+        """removes one ore more cards from hand
         assumes all cards are in hand, error if not
         cards: list
-        '''
+        """
         assert self.hand[color] >= numColor
         self.hand[color] -= numColor
         
     #add card to hand
     def addCardToHand(self, card):
-        '''adds a single card to hand
+        """adds a single card to hand
         assumes card is a valid choice
         card: String
-        '''
+        """
         if card != None:
             self.hand[card] += 1
     
     #add ticket to hand
     def addTicket(self, ticket):
-        '''adds a single ticket to tickets
+        """adds a single ticket to tickets
         ticket: tuple(city1, city2, value)
-        '''
+        """
         self.tickets[ticket] = False
     
     def completeTicket(self, ticket):
-        '''updates the value in the tickets dict to True for key: ticket
+        """updates the value in the tickets dict to True for key: ticket
         ticket: tuple(city1, city2, value)
-        '''
+        """
         assert ticket in self.tickets
         self.tickets = True
     
@@ -75,10 +82,15 @@ class Player:
         self.numTrains -= numTrains
         
     def setPlayerName(self, name):
-        '''sets playerName to name
+        """sets playerName to name
         name: string
-        '''
+        """
         self.name = name
     
     def getName(self):
         return self.name
+        
+
+        
+        
+        
